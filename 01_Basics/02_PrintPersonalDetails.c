a8411d9add075030a4b5a79077f1b7908e651f66
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-03 12:58:04 +0530

    Update 02_PrintPersonalDetails.c
    Added program to print personal details

--- 01_Basics/02_PrintPersonalDetails.c
+++ 01_Basics/02_PrintPersonalDetails.c
@@ -7,6 +7,5 @@ int main()
     printf("Branch      : Electronics and Communication Engineering\n");
     printf("USN         : 1SB23EC024\n");
     printf("City        : Bangalore\n");
-
     return 0;
 }
