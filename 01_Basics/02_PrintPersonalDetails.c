ff92e5a63bdf2d11d2590cea2c7f08488cfc6fc6
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-03 12:55:06 +0530

    Create 02_PrintPersonalDetails.c

---
+++ 01_Basics/02_PrintPersonalDetails.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name        : G M Sagar\n");
+    printf("College     : Sri Sairam College of Engineering\n");
+    printf("Branch      : Electronics and Communication Engineering\n");
+    printf("USN         : 1SB23EC024\n");
+    printf("City        : Bangalore\n");
+
+    return 0;
+}
