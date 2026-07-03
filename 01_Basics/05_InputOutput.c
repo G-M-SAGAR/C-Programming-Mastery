ac4d49ab3335a3358719606681d0cf51dcac34d7
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-03 13:06:19 +0530

    Create 05_InputOutput.c

---
+++ 01_Basics/05_InputOutput.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float percentage;
+    char grade;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your percentage: ");
+    scanf("%f", &percentage);
+
+    printf("Enter your grade: ");
+    scanf(" %c", &grade);
+
+    printf("\n----- Student Details -----\n");
+    printf("Age        : %d\n", age);
+    printf("Percentage : %.2f\n", percentage);
+    printf("Grade      : %c\n", grade);
+
+    return 0;
+}
