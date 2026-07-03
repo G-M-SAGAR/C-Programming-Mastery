02075510d0114c884eb890e3b572b1de957b8f1e
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-03 13:41:14 +0530

    Create 23_SwapWithoutThirdVariable.c
    Added Swap without Third Variable program

---
+++ 01_Basics/23_SwapWithoutThirdVariable.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter first number (a): ");
+    scanf("%d", &a);
+
+    printf("Enter second number (b): ");
+    scanf("%d", &b);
+
+    printf("\nBefore Swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("\nAfter Swapping:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
