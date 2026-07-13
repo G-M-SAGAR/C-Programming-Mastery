ccc271c5a66e2857b0bfcc7822cd7530c313f02b
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 16:23:09 +0530

    Create 13_InsertionSort.c

---
+++ 06_Arrays/13_InsertionSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    int i, j, key;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 1; i < 5; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    printf("\nSorted Array:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
