44e0888720eef76fcd95f4a773fb334f386b4a9b
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 10:09:24 +0530

    Create 02_RelationalOperators.c

---
+++ 02_Operators/02_RelationalOperators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nRelational Operator Results\n");
+    printf("----------------------------\n");
+
+    printf("%d == %d : %d\n", a, b, a == b);
+    printf("%d != %d : %d\n", a, b, a != b);
+    printf("%d > %d  : %d\n", a, b, a > b);
+    printf("%d < %d  : %d\n", a, b, a < b);
+    printf("%d >= %d : %d\n", a, b, a >= b);
+    printf("%d <= %d : %d\n", a, b, a <= b);
+
+    return 0;
+}
