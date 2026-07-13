142712fa63d94f851f47665d39ef0c9b74b7410d
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 16:17:20 +0530

    Create 01_ArrayInputOutput.c

---
+++ 06_Arrays/01_ArrayInputOutput.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    int i;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nArray Elements are:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
