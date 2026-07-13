4a743401b21992bd234541b6cb81670b36aea782
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 16:48:50 +0530

    Create 04_StringConcatenation.c

---
+++ 07_Strings/04_StringConcatenation.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char str1[200];
+    char str2[100];
+    int i = 0, j = 0;
+
+    printf("Enter first string: ");
+    scanf("%99s", str1);
+
+    printf("Enter second string: ");
+    scanf("%99s", str2);
+
+    while(str1[i] != '\0')
+    {
+        i++;
+    }
+
+    while(str2[j] != '\0')
+    {
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+
+    str1[i] = '\0';
+
+    printf("Concatenated String: %s\n", str1);
+
+    return 0;
+}
