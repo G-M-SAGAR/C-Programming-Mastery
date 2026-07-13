0d1f395c31e7c8c60ca19ae78e54618309b0172f
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 10:09:50 +0530

    Create 06_BitwiseOperators.c

---
+++ 02_Operators/06_BitwiseOperators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nBitwise Operator Results\n");
+    printf("------------------------\n");
+
+    printf("a & b = %d\n", a & b);
+    printf("a | b = %d\n", a | b);
+    printf("a ^ b = %d\n", a ^ b);
+    printf("~a = %d\n", ~a);
+    printf("~b = %d\n", ~b);
+
+    return 0;
+}
