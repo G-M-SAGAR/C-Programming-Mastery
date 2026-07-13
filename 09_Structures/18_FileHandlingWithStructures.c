fa4d1d8d64f699e07dd8467c970f0bf88675e97b
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 18:02:47 +0530

    Create 18_FileHandlingWithStructures.c

---
+++ 09_Structures/18_FileHandlingWithStructures.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student
+{
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    FILE *fp;
+    struct Student s;
+
+    fp = fopen("students.txt", "w");
+
+    if(fp == NULL)
+    {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter Name: ");
+    scanf("%49s", s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    fprintf(fp, "%d %s %.2f\n", s.roll, s.name, s.marks);
+
+    fclose(fp);
+
+    printf("Record saved successfully.\n");
+
+    return 0;
+}
