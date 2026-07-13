461c49a890b3ca6b03b164f9de99389c2d251cba
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 15:32:25 +0530

    Create 01_FunctionWithoutArgumentsWithoutReturn.c

---
+++ 05_Functions/01_FunctionWithoutArgumentsWithoutReturn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Function Declaration
+void greet();
+
+// Main Function
+int main()
+{
+    greet();   // Function Call
+
+    return 0;
+}
+
+// Function Definition
+void greet()
+{
+    printf("Welcome to C Programming!\n");
+    printf("Functions make programs modular.\n");
+}
