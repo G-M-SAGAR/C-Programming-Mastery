10d54cea3eef983621f537ba2d2311f0f6e3d48b
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 17:24:53 +0530

    Create 12_PointersToStructures.c

---
+++ 08_Pointers/12_PointersToStructures.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Student
+{
+    int roll;
+    char name[30];
+    float marks;
+};
+
+int main()
+{
+    struct Student s = {101, "Rahul", 91.5};
+
+    struct Student *ptr = &s;
+
+    printf("Roll No : %d\n", ptr->roll);
+    printf("Name    : %s\n", ptr->name);
+    printf("Marks   : %.2f\n", ptr->marks);
+
+    return 0;
+}
