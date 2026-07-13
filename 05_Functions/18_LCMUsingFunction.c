e1faa13dca75085faa5e4550df5172a84308c018
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 15:50:10 +0530

    Create 18_LCMUsingFunction.c

---
+++ 05_Functions/18_LCMUsingFunction.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int gcd(int, int);
+int lcm(int, int);
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("LCM = %d\n", lcm(a, b));
+
+    return 0;
+}
+
+int gcd(int a, int b)
+{
+    while(b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
