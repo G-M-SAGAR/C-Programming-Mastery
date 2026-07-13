61b705fdb67e129e90b87db8031a417c0b196533
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 15:24:10 +0530

    Update 18_ProfitOrLossCalculator.c

---
+++ 03_Conditional_Statements/18_ProfitOrLossCalculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    float costPrice, sellingPrice, amount;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &costPrice);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sellingPrice);
+
+    if(sellingPrice > costPrice)
+    {
+        amount = sellingPrice - costPrice;
+        printf("Profit = Rs. %.2f\n", amount);
+    }
+    else if(costPrice > sellingPrice)
+    {
+        amount = costPrice - sellingPrice;
+        printf("Loss = Rs. %.2f\n", amount);
+    }
+    else
+    {
+        printf("No Profit No Loss.\n");
+    }
+
+    return 0;
+}
