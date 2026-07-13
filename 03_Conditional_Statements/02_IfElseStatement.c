d737b0535e5e49b42bf9fd00a794db50333110a9
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-13 15:22:52 +0530

    Update 02_IfElseStatement.c

---
+++ 03_Conditional_Statements/02_IfElseStatement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num >= 0)
+    {
+        printf("%d is a Positive Number.\n", num);
+    }
+    else
+    {
+        printf("%d is a Negative Number.\n", num);
+    }
+
+    return 0;
+}
