5c5e718a2105fdd9949409e7c29d4e9b42e909a3
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:11:12 +0530

    Create 02_DisplayLinkedList.c

---
+++ 16_LinkedLists/02_DisplayLinkedList.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d", &newNode->data);
+
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    printf("\nLinked List:\n");
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+
+    return 0;
+}
