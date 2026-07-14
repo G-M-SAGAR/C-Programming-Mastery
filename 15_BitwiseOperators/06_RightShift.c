2c773cf0544dd7e4dc50a5d1fa0363617e4e34b1
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 08:46:51 +0530

    Create 06_RightShift.c

---
+++ 15_BitwiseOperators/06_RightShift.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, shift;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Enter shift count: ");
+    scanf("%d", &shift);
+
+    printf("Result = %d\n", num >> shift);
+
+    return 0;
+}
