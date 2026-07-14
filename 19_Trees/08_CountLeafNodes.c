129a733216e17a0b2dc32e9a7ad5e6bd0a6c2029
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:12:20 +0530

    Create 08_CountLeafNodes.c

---
+++ 19_Trees/08_CountLeafNodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int countLeafNodes(struct Node *root)
+{
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+int main()
+{
+    struct Node *root = createNode(10);
+    root->left = createNode(20);
+    root->right = createNode(30);
+    root->left->left = createNode(40);
+    root->left->right = createNode(50);
+
+    printf("Leaf Nodes = %d\n", countLeafNodes(root));
+
+    return 0;
+}
