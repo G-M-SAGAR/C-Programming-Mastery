37400ad544c3dec0c2eeef598075950428099d67
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:12:44 +0530

    Create 13_BinarySearchTreeDeletion.c

---
+++ 19_Trees/13_BinarySearchTreeDeletion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("BST Deletion Cases\n\n");
+
+    printf("1. Delete Leaf Node\n");
+    printf("2. Delete Node with One Child\n");
+    printf("3. Delete Node with Two Children\n");
+
+    printf("\n(Complete BST deletion implementation can be added in advanced version.)\n");
+
+    return 0;
+}
