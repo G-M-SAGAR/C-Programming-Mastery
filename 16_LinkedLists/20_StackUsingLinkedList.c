ce380222c2653dda59e8fe46402e27bb3440801c
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:12:47 +0530

    Create 20_StackUsingLinkedList.c

---
+++ 16_LinkedLists/20_StackUsingLinkedList.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Stack Using Linked List\n");
+    printf("Push -> Insert at Beginning\n");
+    printf("Pop -> Delete from Beginning\n");
+
+    return 0;
+}
