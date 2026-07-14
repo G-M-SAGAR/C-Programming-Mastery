f8ff33c3106d144677c7a6d46c42fc0fbbcf9ad1
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 08:04:13 +0530

    Create 15_DanglingPointerExample.c

---
+++ 13_DynamicMemoryAllocation/15_DanglingPointerExample.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+
+    ptr = (int *)malloc(sizeof(int));
+
+    if(ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    *ptr = 500;
+
+    printf("Before Free = %d\n", *ptr);
+
+    free(ptr);
+
+    ptr = NULL;
+
+    if(ptr == NULL)
+    {
+        printf("Pointer is NULL. Dangling pointer avoided.\n");
+    }
+
+    return 0;
+}
