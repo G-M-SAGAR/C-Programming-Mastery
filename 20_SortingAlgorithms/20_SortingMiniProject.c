e3e3a0c6cddae64777474bb72856fe93719092a6
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:30:18 +0530

    Create 20_SortingMiniProject.c

---
+++ 20_SortingAlgorithms/20_SortingMiniProject.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter Number of Elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter Elements:\n");
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted Array:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
