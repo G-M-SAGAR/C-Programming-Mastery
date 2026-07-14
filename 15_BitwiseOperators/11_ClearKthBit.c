196fe7651625ff2bdef376aebb1c67e6aa89859d
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 08:47:16 +0530

    Create 11_ClearKthBit.c

---
+++ 15_BitwiseOperators/11_ClearKthBit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, k;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Enter bit position (0-based): ");
+    scanf("%d", &k);
+
+    num = num & ~(1 << k);
+
+    printf("Updated Number = %d\n", num);
+
+    return 0;
+}
