f1fad66c4a5ec7580aa08edeebaf7b567d5e8ffd
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:13:14 +0530

    Create 18_ExpressionTree.c

---
+++ 19_Trees/18_ExpressionTree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Expression Tree Example\n\n");
+
+    printf("        +\n");
+    printf("      /   \\\n");
+    printf("     A     *\n");
+    printf("          / \\\n");
+    printf("         B   C\n");
+
+    printf("\nExpression: A + (B * C)\n");
+
+    return 0;
+}
