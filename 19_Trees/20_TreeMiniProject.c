5e5eb24fb43e2524d466cffeb24059bb1a926736
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:13:26 +0530

    Create 20_TreeMiniProject.c

---
+++ 19_Trees/20_TreeMiniProject.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("===== TREE OPERATIONS =====\n\n");
+
+    printf("1. Create Tree\n");
+    printf("2. Insert Node\n");
+    printf("3. Search Node\n");
+    printf("4. Preorder Traversal\n");
+    printf("5. Inorder Traversal\n");
+    printf("6. Postorder Traversal\n");
+    printf("7. Count Nodes\n");
+    printf("8. Find Height\n");
+    printf("9. Exit\n");
+
+    printf("\nTree Mini Project Completed.\n");
+
+    return 0;
+}
