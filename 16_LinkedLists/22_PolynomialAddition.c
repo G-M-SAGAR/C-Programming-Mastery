4d427ff7c3e7012da901648e807163bffd42727b
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:13:04 +0530

    Create 22_PolynomialAddition.c

---
+++ 16_LinkedLists/22_PolynomialAddition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Polynomial Addition using Linked List\n\n");
+
+    printf("(5x^2 + 4x + 2)\n");
+    printf("+\n");
+    printf("(3x^2 + 6x + 1)\n\n");
+
+    printf("Result:\n");
+    printf("8x^2 + 10x + 3\n");
+
+    return 0;
+}
