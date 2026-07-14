fab2d9b74c34b6718b932c7452cee757ea04cfca
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:57:15 +0530

    Create 15_OutputRestrictedDeque.c

---
+++ 18_Queues/15_OutputRestrictedDeque.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Output Restricted Deque\n");
+
+    printf("Insertion Allowed : Front and Rear\n");
+    printf("Deletion Allowed : Front Only\n");
+
+    return 0;
+}
