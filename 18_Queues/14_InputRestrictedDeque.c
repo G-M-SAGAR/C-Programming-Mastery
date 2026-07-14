3c62e6d8e6174a45416538d6efee527253220532
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:57:11 +0530

    Create 14_InputRestrictedDeque.c

---
+++ 18_Queues/14_InputRestrictedDeque.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Input Restricted Deque\n");
+
+    printf("Insertion Allowed : Rear Only\n");
+    printf("Deletion Allowed : Front and Rear\n");
+
+    return 0;
+}
