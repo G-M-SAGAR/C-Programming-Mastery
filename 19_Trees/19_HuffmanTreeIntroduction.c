c234df828aff109551f0c7ff99e52da631e9e9ff
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:13:20 +0530

    Create 19_HuffmanTreeIntroduction.c

---
+++ 19_Trees/19_HuffmanTreeIntroduction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Huffman Tree\n\n");
+
+    printf("Applications:\n");
+    printf("1. Data Compression\n");
+    printf("2. ZIP Files\n");
+    printf("3. File Encoding\n");
+    printf("4. Lossless Compression\n");
+
+    return 0;
+}
