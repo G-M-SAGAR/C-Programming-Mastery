1426b64f0d4fafc9a4e3cbbee390080ef643c89f
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:12:25 +0530

    Create 09_FindHeightOfTree.c

---
+++ 19_Trees/09_FindHeightOfTree.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int height(struct Node *root)
+{
+    if(root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main()
+{
+    struct Node *root = createNode(10);
+    root->left = createNode(20);
+    root->right = createNode(30);
+    root->left->left = createNode(40);
+    root->left->right = createNode(50);
+
+    printf("Height of Tree = %d\n", height(root));
+
+    return 0;
+}
