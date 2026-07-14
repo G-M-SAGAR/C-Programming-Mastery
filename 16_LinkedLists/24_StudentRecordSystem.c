8b5eff5d18ee9238d5bc96786bded795d8fdd97f
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:13:14 +0530

    Create 24_StudentRecordSystem.c

---
+++ 16_LinkedLists/24_StudentRecordSystem.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+struct Student
+{
+    int roll;
+    char name[30];
+    float marks;
+};
+
+int main()
+{
+    struct Student s[3];
+
+    for(int i=0;i<3;i++)
+    {
+        printf("Enter Roll Name Marks: ");
+        scanf("%d%s%f",&s[i].roll,s[i].name,&s[i].marks);
+    }
+
+    printf("\nStudent Records\n\n");
+
+    for(int i=0;i<3;i++)
+    {
+        printf("%d\t%s\t%.2f\n",
+               s[i].roll,
+               s[i].name,
+               s[i].marks);
+    }
+
+    return 0;
+}
