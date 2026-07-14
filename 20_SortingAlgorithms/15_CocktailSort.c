1af61e23b9d32800b33035c3d01ae9d12fc65fbe
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 10:29:53 +0530

    Create 15_CocktailSort.c

---
+++ 20_SortingAlgorithms/15_CocktailSort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {5,1,4,2,8};
+    int n = 5;
+
+    int start = 0;
+    int end = n - 1;
+    int swapped = 1;
+
+    while(swapped)
+    {
+        swapped = 0;
+
+        for(int i = start; i < end; i++)
+        {
+            if(arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if(!swapped)
+            break;
+
+        swapped = 0;
+        end--;
+
+        for(int i = end - 1; i >= start; i--)
+        {
+            if(arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        start++;
+    }
+
+    printf("Sorted Array:\n");
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
