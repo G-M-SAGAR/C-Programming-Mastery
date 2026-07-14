7c038e2b0e54c5fb048a0c012fc7f73427b88ccf
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:11:32 +0530

    Create 06_DeleteFromBeginning.c

---
+++ 16_LinkedLists/06_DeleteFromBeginning.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d", &newNode->data);
+
+        newNode->next = NULL;
+
+        if(head == NULL)
+            head = temp = newNode;
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    if(head == NULL)
+    {
+        printf("List is Empty.\n");
+        return 0;
+    }
+
+    temp = head;
+    head = head->next;
+    free(temp);
+
+    printf("\nAfter Deletion:\n");
+
+    temp = head;
+
+    while(temp)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+
+    return 0;
+}
