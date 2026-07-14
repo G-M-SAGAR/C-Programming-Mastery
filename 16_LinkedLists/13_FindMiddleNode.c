335acae50a48b515fce42ebc468c669068aa522c
G M SAGAR <gmsagar1bro@gmail.com>
2026-07-14 09:12:05 +0530

    Create 13_FindMiddleNode.c

---
+++ 16_LinkedLists/13_FindMiddleNode.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head=NULL,*temp=NULL,*newNode;
+    struct Node *slow,*fast;
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        newNode=(struct Node*)malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d",&newNode->data);
+
+        newNode->next=NULL;
+
+        if(head==NULL)
+            head=temp=newNode;
+        else
+        {
+            temp->next=newNode;
+            temp=newNode;
+        }
+    }
+
+    slow=fast=head;
+
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+
+    printf("Middle Node = %d\n",slow->data);
+
+    return 0;
+}
